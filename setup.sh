c6ada21e536b7efb7ec4ec3480f719bdf9850274
i3onilha <73854026+i3onilha@users.noreply.github.com>
2022-11-24 00:46:58 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,13 @@ cd ~ && /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local
 cd ~ && /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME reset HEAD .
 cd ~ && /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout -- .
 cd ~/.vim/bundle/coc.nvim/ && npm install
+git clone --bare -b last-stable https://github.com/jean-bonilha/.dotfiles.git $HOME/.dotfiles && \
+git clone https://github.com/jean-bonilha/.vim.git $HOME/.vim && \
+git clone https://github.com/jean-bonilha/.tmux.git $HOME/.tmux && \
+ln -sf .vim/.vimrc $HOME && \
+ln -sf .tmux/.tmux.conf $HOME && \
+cp $HOME/.tmux/.tmux.conf.local $HOME && \
+cd ~/.vim && \
+git checkout heavenly2 && \
+git submodule init && \
+git submodule update && \
