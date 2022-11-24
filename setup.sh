6e06237fb9a30018938adb86fa9d882b25b3c9ee
i3onilha <73854026+i3onilha@users.noreply.github.com>
2022-11-24 00:12:15 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ cd ~/.vim/bundle/coc.nvim/ && yarn
 cd ~ && /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
 cd ~ && /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME reset HEAD .
 cd ~ && /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout -- .
+cd ~/.vim/bundle/coc.nvim/ && yarn
