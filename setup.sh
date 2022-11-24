29f8abe8bd48bb27bb551a8be8c8b658c8f4a629
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2022-11-24 00:22:01 -0400

    master#init commit

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd ~/.vim/bundle/coc.nvim/ && yarn
+cd ~ && /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
+cd ~ && /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME reset HEAD .
+cd ~ && /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout -- .
